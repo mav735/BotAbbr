35651517ccfae5612b5baafc2194caadd6e433fe
mav735 <mav735@ya.ru>
2024-10-25 19:35:48 +0300

    docker upd, code restyle

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # BotAbbr
+
+A Telegram bot that helps users quickly find the meanings of various abbreviations. This bot listens for abbreviation queries and responds with definitions from a predefined list. The bot also allows the administrator to update the abbreviation list directly by sending a file.
+
+## Requirements
+
+- Ubuntu (tested on Ubuntu 20.04 and 22.04)
+- Docker
+- Telegram Bot API Token
+
+## Installation Instructions
+
+### Step 1: Clone the Repository
+
+First, clone this repository to your local machine:
+
+```bash
+git clone https://github.com/mav735/BotAbbr.git
+cd BotAbbr
+```
+
+### Step 2: Install Docker on Ubuntu
+To run this project in a Docker container, Docker needs to be installed on your Ubuntu system. Follow these steps to install Docker:
+
+Update your existing package list:
+
+```bash
+sudo apt update
+```
+
+Add Docker’s official GPG key:
+
+```bash
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+```
+
+```bash
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+```
+
+Update the package database and install Docker:
+
+```bash
+sudo apt update
+sudo apt install docker-ce -y
+```
+
+### Step: 3 Build + Run Docker container
+
+```bash
+docker build -t botabbr -f Dockerfile .
+docker run -d --name botabbr-container botabbr
+```
--- bot.py
+++ bot.py
@@ -1,46 +1,66 @@
 import telebot
 import time
 from requests.exceptions import RequestException
-bot = telebot.TeleBot('7060695421:AAGhq8DTppMGjYsf197ZWJAzCA01Iqpydec');
 
-f = open('abbr.txt','r', encoding='UTF-8')
-abbrs = f.read().split('\n')
-f.close()
+bot = telebot.TeleBot('7060695421:AAGhq8DTppMGjYsf197ZWJAzCA01Iqpydec')
 
+with open('abbr.txt', 'r', encoding='UTF-8') as f:
+    abbrs = f.read().split('\n')
 
 @bot.message_handler(commands=["start"])
-def start(m,res=False):
-   bot.send_message(m.chat.id, 'Привет! \n'
-                               'Я - бот-Вордбук 🤖\n'
-                               '- твой интеллектуальный помощник в распознавании аббревиатур.\n \n'
-                               'Помогу быстро найти значения различных сокращений и аббревиатур, используемых в рабочем процессе. \n'
-                               'Бот-словарь станет надежным спутником в рабочем процессе, облегчая коммуникацию и повышая эффективность взаимодействия с коллегами и смежными подразделениями💯')
-   bot.send_message(m.chat.id, 'Введите аббревиатуру, значение которой хотите узнать👇')
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Привет! \n'
+                                'Я - бот-Вордбук 🤖\n'
+                                '- твой интеллектуальный помощник в распознавании аббревиатур.\n \n'
+                                'Помогу быстро найти значения различных сокращений и аббревиатур, используемых в рабочем процессе. \n'
+                                'Бот-словарь станет надежным спутником в рабочем процессе, облегчая коммуникацию и повышая эффективность взаимодействия с коллегами и смежными подразделениями💯')
+    bot.send_message(m.chat.id, 'Введите аббревиатуру, значение которой хотите узнать👇')
+
+@bot.message_handler(commands=["send_abbr_file"])
+def send_abbr_file(m):
+    if m.chat.id == 431436587:
+        with open('abbr.txt', 'rb') as file:
+            bot.send_document(m.chat.id, file)
+    else:
+        bot.send_message(m.chat.id, "Извините, у вас нет доступа к этой команде.")
+
+@bot.message_handler(content_types=['document'])
+def receive_abbr_file(message):
+    if message.chat.id == 431436587 and message.document.file_name == 'abbr.txt':
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open('abbr.txt', 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.send_message(message.chat.id, "Файл abbr.txt был успешно обновлен!")
+        reload_abbrs()
+
+def reload_abbrs():
+    global abbrs
+    with open('abbr.txt', 'r', encoding='UTF-8') as f:
+        abbrs = f.read().split('\n')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    countMess = 0
-    flag = False
-    for abbr in abbrs:
-        if flag:
-            bot.send_message(message.from_user.id, abbr)
-            countMess = 1
-            if abbr == '':
-                break
-        if abbr == message.text.lower():
-            flag = True
-    if countMess == 0:
+    try:
+        index_start = abbrs.index(message.text.lower())
+        try:
+            index_stop = abbrs.index('', index_start)
+            need_part = abbrs[index_start + 1:index_stop + 1]
+            new_message = '\n'.join(need_part)
+        except ValueError:
+            new_message = abbrs[index_start + 1]
+        bot.send_message(message.from_user.id, new_message)
+    except ValueError:
         chat_id = 431436587
         bot.send_message(message.from_user.id, 'Я не знаю такой аббревиатуры😞 \n'
-                                                   'Но уже отправил Ваш запрос создателю бота для пополнения базы знаний!')
+                                               'Но уже отправил Ваш запрос создателю бота для пополнения базы знаний!')
         bot.send_message(chat_id, 'Пользователь пытался найти аббревиатуру: ' + message.text)
-
 
 bot.polling(none_stop=True, interval=0)
 
 while True:
     try:
-        bot.infinity_polling(timeout=10, long_polling_timeout = 5)
+        bot.infinity_polling(timeout=10, long_polling_timeout=5)
     except RequestException as err:
         print(err)
         print('* Connection failed, waiting to reconnect...')
